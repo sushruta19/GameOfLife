4cc969c1faf3ca658fdc30a05e5b289c1411abf1
Soubhik Sen <sensoubhik2001@gmail.com>
2026-02-24 11:55:29 +0530

    Fix simulation time, Display Info, Window size
    Fixed accumulator variable since generations were getting skipped since accumulator was adding up time
    even during pause.
    Set self.paused to True during keystrokes and giving input
    Add display information
    Made window resizable and accordingly changed offsets
    Only pygame display and font initialised as it was taking too much time
    to load pygame.init()

--- renderer.py
+++ renderer.py
@@ -7,20 +7,30 @@ from typing import Tuple
 from engine import Universe
 
 class Renderer:
-    def __init__(self, universe: Universe, width: int = 1000, height:int = 800) -> None:
-        pygame.init()
-        self.universe = universe
-        self.width = width
-        self.height = height
+    def __init__(self, universe: Universe) -> None:
+        print("Initializing pygame...")
+        pygame.display.init()
+        pygame.font.init()
+        print("Pygame initialized.")
 
-        self.screen = pygame.display.set_mode((width, height))
+        self.universe = universe
+        info = pygame.display.Info()
+        self.width = int(info.current_w*0.8)
+        self.height = int(info.current_h*0.8)
+        
+        print("Creating window...")
+        self.screen = pygame.display.set_mode(
+            (self.width, self.height),
+            pygame.RESIZABLE    
+        )
+        print("Window created.")
         pygame.display.set_caption("Conway's Game of Life")
 
         self.clock = pygame.time.Clock()
 
         self.cell_size = 20 #20px height 20px width
-        self.offset_x = width // 2
-        self.offset_y = height // 2
+        self.offset_x = self.width // 2
+        self.offset_y = self.height // 2
 
         self.bg_color = (15, 15, 15)
         self.cell_color = (0, 200, 120)
@@ -30,6 +40,8 @@ class Renderer:
 
         self.running = True
         self.paused = True
+
+        self.font = pygame.font.Font(None, 20)
     
     def world_to_screen(self, x: int, y: int) -> Tuple[int, int]:
         sx = x*self.cell_size + self.offset_x
@@ -41,6 +53,19 @@ class Renderer:
         y = (sy - self.offset_y) // self.cell_size        
         return x, y
 
+    def display_info(self) -> None:
+        """Display Info about population, generation etc"""
+        population = self.universe.population()
+        generation = self.universe.generation
+
+        info_text = (
+            f"Generation: {generation}   "
+            f"Population: {population}   "
+            f"Speed: {1/self.simulation_interval:.2f} gen/s"
+        )
+        text_surface = self.font.render(info_text, True, (200, 200, 200))
+        self.screen.blit(text_surface, (10, 10))
+        
     def draw(self) -> None:
         self.screen.fill(self.bg_color)
 
@@ -48,7 +73,7 @@ class Renderer:
             sx, sy = self.world_to_screen(x, y)
             rect = pygame.Rect(sx, sy, self.cell_size, self.cell_size)
             pygame.draw.rect(self.screen, self.cell_color, rect)
-        
+        self.display_info()
         #bringing all changes to actual screen display
         pygame.display.flip()
 
@@ -60,11 +85,18 @@ class Renderer:
                 if event.key == pygame.K_SPACE:
                     self.paused = not self.paused
                 elif event.key == pygame.K_c:
+                    self.paused = True
                     self.universe.clear()
             elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.paused = True
                 mx, my = pygame.mouse.get_pos()
                 x, y = self.screen_to_world(mx, my)
                 self.universe.toggle_cell(x, y)
+            elif event.type == pygame.VIDEORESIZE:
+                self.width, self.height = event.size
+                self.screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
+                self.offset_x = self.width // 2
+                self.offset_y = self.height // 2
 
     def run(self) -> None:
         target_fps = 30
@@ -72,14 +104,16 @@ class Renderer:
         while self.running:
             # how much time last frame took(ms->s)
             dt = self.clock.tick(target_fps)/1000.0
-            self.accumulator += dt
 
             self.handle_events()
 
             if not self.paused:
+                self.accumulator += dt
                 while self.accumulator >= self.simulation_interval:
                     self.universe.step()
                     self.accumulator -= self.simulation_interval
+            else:
+                self.accumulator = 0.0
             self.draw()
 
         pygame.quit()
